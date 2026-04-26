b5dbb017988bee27642fb2a52ba018fd2448c8ab
Sakshamgoyal02 <sakshamgoyal142@gmail.com>
2026-04-27 02:56:54 +0530

    Implemented HeroSection, MessageSection and FlavorSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import Navbar from './components/Navbar'
-import {ScrollTrigger} from 'gsap/all';
+import {ScrollSmoother, ScrollTrigger} from 'gsap/all';
 import gsap from 'gsap';
+import HeroSection from './sections/HeroSection';
+import MessageSection from './sections/MessageSection';
+import FlavorSection from './sections/FlavorSection';
+import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+     ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+     });
+  });
+
   return (
     <>
      <Navbar/>
+     <div id='smooth-wrapper'>
+      <div id='smooth-content'>
+        <HeroSection/>
+        <MessageSection/>
+        <FlavorSection/>
+      </div>
+     </div>
+    
     </>
   )
 }
